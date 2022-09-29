ef09d241bdad471d855a224844705aaeac70c533
surajkishordas <suraj18may@gmail.com>
2022-09-29 14:27:05 +0530

    my first java commit

---
+++ src/testometer_java_training/Test1.java
@@ -0,0 +1,19 @@
+package testometer_java_training;
+
+public class Test1 {
+public static void main(String[] args) {
+	System.out.println("12345");
+}
+}
+
+	class A
+	{
+}
+   class B
+   {
+	   
+   }
+     class C {
+
+    	 
+     }
